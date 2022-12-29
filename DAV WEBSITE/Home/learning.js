27dcb5f453f629975fb08f7068db008e7c3091e8
Abhinav1021 <119749349+Abhinav1021@users.noreply.github.com>
2022-12-29 08:34:25 +0000

    changed the navbar

--- DAV WEBSITE/Home/learning.js
+++ DAV WEBSITE/Home/learning.js
@@ -1,11 +1,11 @@
-                    // Footer
-function sendEmail(){
+// Footer
+function sendEmail() {
     Email.send({
         SecureToken: "bada3c01-2766-417c-98f4-d65070809fe7",
-        To : 'beautyravi25@gmail.com',
-        From : document.getElementById("email").value,
-        Subject : "This is the subject",
-        Body : "Name: " + document.getElementById("name").value 
+        To: 'beautyravi25@gmail.com',
+        From: document.getElementById("email").value,
+        Subject: "This is the subject",
+        Body: "Name: " + document.getElementById("name").value
             + "<br> Email: " + document.getElementById('email').value
             + "<br> Phone no.: " + document.getElementById('phone').value
             + "<br> Message: " + document.getElementById('message').value
@@ -13,4 +13,14 @@ function sendEmail(){
         message => alert("Message Sent Succesfully")
     );
 }
-                                        // Footer end
+// Footer end
+// nav bar
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-list')
+
+burger.addEventListener('click', ()=>{
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
+// nav bar end
--- try/index.js
+++ try/index.js
@@ -1,16 +1,11 @@
-                    // Footer
-function sendEmail(){
-    Email.send({
-        SecureToken: "bada3c01-2766-417c-98f4-d65070809fe7",
-        To : 'beautyravi25@gmail.com',
-        From : document.getElementById("email").value,
-        Subject : "This is the subject",
-        Body : "Name: " + document.getElementById("name").value 
-            + "<br> Email: " + document.getElementById('email').value
-            + "<br> Phone no.: " + document.getElementById('phone').value
-            + "<br> Message: " + document.getElementById('message').value
-    }).then(
-        message => alert("Message Sent Succesfully")
-    );
-}
-                    // Footer end
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-list')
+
+
+
+
+burger.addEventListener('click', ()=>{
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
