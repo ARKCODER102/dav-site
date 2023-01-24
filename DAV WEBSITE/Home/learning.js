90d1667c58f5be0304b4be5714bef504f85eaa08
Abhinav1021 <119749349+Abhinav1021@users.noreply.github.com>
2023-01-24 17:14:43 +0000

    done

--- DAV WEBSITE/Home/learning.js
+++ DAV WEBSITE/Home/learning.js
@@ -26,3 +26,24 @@ hamburger.onclick = function () {
 }
 // nav bar end
 
+const swiper = new Swiper('.swiper', {
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
