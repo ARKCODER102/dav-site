5109f66f0a6808d38c3d21b318e1c8ede7be3c97
Abhinav1021 <119749349+Abhinav1021@users.noreply.github.com>
2023-01-30 07:53:44 +0000

    form done

--- DAV WEBSITE/Home/learning.js
+++ DAV WEBSITE/Home/learning.js
@@ -2,18 +2,121 @@
 function sendEmail(){
     Email.send({
         SecureToken: "12ea53c7-cbc1-4659-86b4-a086803008fa",
-        To : 'aaryan.raj.kumar25@gmail.com',
-        From : document.getElementById('email').value,
-        Subject : document.getElementById('subject').value,
-        Body : "Name: " + document.getElementById('name').value
-            +"<br> Email: " + document.getElementById('email').value
-            +"<br> Phone No.: " + document.getElementById('phone').value
-            +"<br> Message: " + document.getElementById('message').value
+        To : 'beautyravi25@gmail.com',
+        From : document.getElementById('contact-email').value,
+        Subject : document.getElementById('contact-subject').value,
+        Body : "Name: " + document.getElementById('contact-name').value
+            +"<br> Email: " + document.getElementById('contact-email').value
+            +"<br> Phone No.: " + document.getElementById('contact-phone').value
+            +"<br> Message: " + document.getElementById('contact-message').value
     }).then(
     message => alert("Message sent successfully")
     );
 }
 
+var nameError = document.getElementById('name-error');
+var phoneError = document.getElementById('phone-error');
+var emailError = document.getElementById('email-error');
+var subjectError = document.getElementById('subject-error');
+var messageError = document.getElementById('message-error');
+var submitError = document.getElementById('submit-error');
+
+function validateName() {
+    var name = document.getElementById('contact-name').value;
+
+    if (name.length == 0) {
+        nameError.innerHTML = 'Name is required';
+        return false;
+    }
+
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = 'Write full name';
+        return false;
+    }
+    nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validatePhone(){
+    var phone = document.getElementById('contact-phone').value;
+
+    if (phone.length == 0) {
+        phoneError.innerHTML = "Phone no is required";
+        return false;
+    }
+
+    if (phone.length !== 10) {
+        phoneError.innerHTML = "Phone number should be 10 characters";
+        return false;
+    }
+
+    if (!phone.match(/^[0-9]{10}$/)){
+        phoneError.innerHTML = "Phone no is required";
+        return false;
+    }
+
+    phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+function validateEmail(){
+    var email = document.getElementById('contact-email').value;
+
+    if (email.length == 0){
+        emailError.innerHTML = 'Email is required';
+        return false;
+    }
+
+    if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = 'Email invalid';
+        return false;
+    }
+
+    emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateMessage(){
+    var message = document.getElementById('contact-message').value;
+    var required = 30;
+    var left = required - message.length;
+
+    if (left > 0){
+        messageError.innerHTML = left + ' more characters required';
+        return false;
+    }
+
+    messageError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateForm(){
+    if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage() || !validateSubject()){
+        submitError.style.display = 'block';
+        submitError.innerHTML = "Please fix error to submit form";
+        setTimeout(function(){submitError.style.display = 'none';}, 3000);
+        return false;
+    }
+
+}
+
+function validateSubject(){
+    var subject = document.getElementById('contact-subject').value;
+    var required = 20;
+    var left = required - subject.length;
+
+    if (subject == 0){
+        subjectError.innerHTML = "Please write a subject";
+        return false;
+    }
+
+    if (left > 0){
+        subjectError.innerHTML = left + " more characters required";
+        return false;
+    }
+
+    subjectError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
 // Footer end
 // nav bar
 burger = document.querySelector(".burger");
